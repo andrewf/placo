ec5d5f338b609f602a9be88ad01f12bc6f059535
Andrew Fleenor <andrewflnr@gmail.com>
2025-07-13 15:20:06 -0400

    move unit tests into their actual files
    drracket runs these just fine. no need for extra files.

--- parse.rkt
+++ parse.rkt
@@ -95,7 +95,7 @@
                   (paren-expr s)
                   (if-expr s)
                   (fun-expr s)
-                  (ident s)
+                  (ident s)  ; at end so it doesn't parse keywords as vars
                   #f)])
     ; parse optional postfix clause (fn call parens, etc)
     (if main
@@ -151,3 +151,55 @@
     (if p
         (list 'var p)
         #f)))
+
+(module+ test
+
+(require rackunit)
+
+(check-equal? (parse (open-input-string "  "))
+              '())
+
+(check-equal? (parse (open-input-string "def cd(a) 42 end"))
+              '((def (ident "cd") (ident "a") (lit 42))))
+
+; lexer is stupid, so space between parens is mandatory
+(check-equal? (parse (open-input-string "def cd( ) 42 end"))
+              '((def (ident "cd") #f (lit 42))))
+
+(check-equal? (parse (open-input-string "def cd( a) 42 end def fred(z) 13 end"))
+              '((def (ident "cd") (ident "a") (lit 42))
+                (def (ident "fred") (ident "z") (lit 13))))
+
+(check-equal? (parse (open-input-string "let abc=42"))
+              '((let (ident "abc") (lit 42))))
+
+(check-equal? (parse (open-input-string "let abc=42"))
+              '((let (ident "abc") (lit 42))))
+
+
+(check-equal? (parse (open-input-string "let abc= (3) let x=3"))
+              '((let (ident "abc") (lit 3)) ; paren-expr unwraps itself
+                (let (ident "x") (lit 3))))
+
+(check-equal? (parse (open-input-string "let abc= if x then (3) end let x=3"))
+              '((let (ident "abc") (if (ident "x") (lit 3) #f))
+                (let (ident "x") (lit 3))))
+
+(check-equal? (parse (open-input-string "let abc= ( ( ( x) ) )"))
+              '((let (ident "abc") (ident "x"))))
+
+(check-equal? (parse (open-input-string "let abc= if x then (3) else if 4 then ( ( (x) ) ) end end let x=3"))
+              '((let (ident "abc") (if (ident "x") (lit 3)
+                                       (if (lit 4) (ident "x") #f)))
+                (let (ident "x") (lit 3))))
+
+(check-equal? (parse (open-input-string "let abc= fun (x) if x then 3 else 4 end end"))
+              '((let (ident "abc")
+                  (fun (ident "x")
+                       (if (ident "x")
+                           (lit 3)
+                           (lit 4))))))
+
+(check-equal? (parse (open-input-string "let abc = f ( ( 4 ) )"))
+              '((let (ident "abc") (funcall (ident "f") (lit 4)))))
+)
--- tok.rkt
+++ tok.rkt
@@ -79,3 +79,39 @@
   (in-producer (tokenize text-stream)
                (lambda (t)
                  (eq? (token-kind t) 'end))))
+
+(module+ test
+(require rackunit)
+
+(check-equal? (sequence->list (token-sequence (open-input-string "a-b b_c")))
+              (list (token 'symbolic "a-b")
+                    (token 'symbolic "b_c"))
+              "separates words")
+
+(check-equal? (sequence->list (token-sequence (open-input-string "fred(")))
+              (list (token 'symbolic "fred")
+                    (token 'operator "("))
+              "separates operators")
+
+(check-equal? (sequence->list (token-sequence (open-input-string "a3b 34 bc")))
+              (list (token 'symbolic "a3b")
+                    (token 'numeric 34)
+                    (token 'symbolic "bc"))
+              "supports numbers")
+
+(check-equal? (sequence->list (token-sequence (open-input-string "a3+b 34/bc")))
+              (list (token 'symbolic "a3")
+                    (token 'operator "+")
+                    (token 'symbolic "b")
+                    (token 'numeric 34)
+                    (token 'operator "/")
+                    (token 'symbolic "bc"))
+              "supports numbers")
+
+(check-exn exn:fail?
+           (lambda () (sequence->list (token-sequence (open-input-string "a3b 34bc")))))
+
+(check-exn exn:fail?
+           (lambda () (sequence->list (token-sequence (open-input-string "a3b 3.4.5")))))
+
+)
