7714560d30eeb446a0da4ae30ebcf5ef92b1c2af
Andrew Fleenor <andrewflnr@gmail.com>
2025-07-20 01:31:30 -0400

    take advantage of better op tokenization to make less-stupid parse tests

--- parse.rkt
+++ parse.rkt
@@ -163,7 +163,7 @@
               '((def (ident "cd") (ident "a") (lit 42))))
 
 ; lexer is stupid, so space between parens is mandatory
-(check-equal? (parse (open-input-string "def cd( ) 42 end"))
+(check-equal? (parse (open-input-string "def cd() 42 end"))
               '((def (ident "cd") #f (lit 42))))
 
 (check-equal? (parse (open-input-string "def cd( a) 42 end def fred(z) 13 end"))
@@ -177,7 +177,7 @@
               '((let (ident "abc") (lit 42))))
 
 
-(check-equal? (parse (open-input-string "let abc= (3) let x=3"))
+(check-equal? (parse (open-input-string "let abc=(3) let x=3"))
               '((let (ident "abc") (lit 3)) ; paren-expr unwraps itself
                 (let (ident "x") (lit 3))))
 
@@ -185,7 +185,7 @@
               '((let (ident "abc") (if (ident "x") (lit 3) #f))
                 (let (ident "x") (lit 3))))
 
-(check-equal? (parse (open-input-string "let abc= ( ( ( x) ) )"))
+(check-equal? (parse (open-input-string "let abc= ( (( x) ))"))
               '((let (ident "abc") (ident "x"))))
 
 (check-equal? (parse (open-input-string "let abc= if x then (3) else if 4 then ( ( (x) ) ) end end let x=3"))
@@ -200,6 +200,6 @@
                            (lit 3)
                            (lit 4))))))
 
-(check-equal? (parse (open-input-string "let abc = f ( ( 4 ) )"))
+(check-equal? (parse (open-input-string "let abc = f (( 4 ))"))
               '((let (ident "abc") (funcall (ident "f") (lit 4)))))
 )
