d0ab663329e6dc1734880a91fdf61e66276866ca
Andrew Fleenor <andrewflnr@gmail.com>
2025-07-21 16:12:39 -0400

    we can add binary functions using manual currying. hurrah.

--- placo.rkt
+++ placo.rkt
@@ -3,7 +3,9 @@
 (require "parse.rkt")
 (require "eval.rkt")
 
-(define prelude-env (bind-env "print" (lambda (arg) (printf "~a\n" arg)) (empty-env)))
+(define prelude-env (bind-env "print" (lambda (arg) (printf "~a\n" arg))
+                              (bind-env "plus" (lambda (rhs) (lambda (lhs) (+ rhs lhs)))
+                              (empty-env))))
 
 (define env (eval-toplevel (parse (current-input-port)) prelude-env))
 
