944559a9b251d29f4f189c173998bf0d88784de7
Andrew Fleenor <andrewflnr@gmail.com>
2025-07-29 12:09:10 -0400

    update actual driver for multiple args
    look, creating both of these files happened late at night.

--- placo.rkt
+++ placo.rkt
@@ -3,10 +3,10 @@
 (require "parse.rkt")
 (require "eval.rkt")
 
-(define prelude-env (bind-env "print" (lambda (arg) (printf "~a\n" arg))
-                              (bind-env "plus" (lambda (rhs) (lambda (lhs) (+ rhs lhs)))
-                              (empty-env))))
+(define prelude-env (bind-env "print" (lambda (args) (printf "~a\n" (car args)))
+                              (bind-env "plus" (lambda (args) (+ (car args) (cadr args)))
+                                        (empty-env))))
 
 (define env (eval-toplevel (parse (current-input-port)) prelude-env))
 
-(eval-expr (funcall (ident "main") #f) env)
+(eval-expr (funcall (ident "main") '()) env)
