870305e37f1d2ababe7da0d641c929d510673c50
Andrew Fleenor <andrewflnr@gmail.com>
2026-04-24 22:43:45 -0400

    use bind-env-names to create prelude in placo driver

--- placo.rkt
+++ placo.rkt
@@ -1,11 +1,14 @@
 #lang racket
 
-(require "parse.rkt")
-(require "eval.rkt")
+(require "parse.rkt" "eval.rkt" "env.rkt")
 
-(define prelude-env (bind-env "print" (lambda (args) (printf "~a\n" (car args)))
-                              (bind-env "plus" (lambda (args) (+ (car args) (cadr args)))
-                                        (empty-env))))
+(define prelude-env (bind-env-names
+                     (list "plus"
+                           "print")
+                     (list
+                      (lambda (args) (+ (car args) (cadr args)))
+                      (lambda (args) (printf "~a\n" (car args))))
+                     (empty-env)))
 
 (define env (eval-toplevel (parse (current-input-port)) prelude-env))
 
