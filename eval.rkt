334208f7ed166d17ab36bf77d0a3c6306c3f9f45
Andrew Fleenor <andrewflnr@gmail.com>
2026-04-24 21:58:08 -0400

    WIP rm env dependency from visitor
    visitor for eval always returns an env->evaluated value thunk.
    Ended up having to fully generalize the toplevel situation with
    visitor-specific reduce functions.

--- eval.rkt
+++ eval.rkt
@@ -12,35 +12,46 @@
 
 (define (truthy t) (not (falsy t)))
 
-(define (eval-visit-funcall fun arg-values env v)
+(define (eval-visit-funcall fun-thunk arg-thunks wrong-env v)
+  (lambda (env)
+    (let ((fun (fun-thunk env))
+          (arg-values (map (lambda (value-thunk) (value-thunk env)) arg-thunks)))
     (if (procedure? fun)
         (fun arg-values)
-        (error (format "trying to call non-function ~a" fun))))
+        (error (format "trying to call non-function ~a" fun))))))
 
-(define (eval-visit-fundef arg-names body-thunk env-at-def v)
+(define (eval-visit-fundef arg-names body-thunk wrong-env-at-def v)
   ; just need to capture lexical context
   ; we can do that with closure in host language. lol.
+  (lambda (env-at-def)
     (lambda (arg-values)
       (let ([actual-env (bind-env-names arg-names arg-values env-at-def)])
-          (body-thunk actual-env))))
+        (body-thunk actual-env)))))
 
-(define (eval-visit-ifexpr condition true-branch else-branch env v)
-    (if (truthy condition)
+(define (eval-visit-ifexpr condition true-branch else-branch wrong-env v)
+  (lambda (env)
+    (if (truthy (condition env))
         (true-branch env)
         (if else-branch
             (else-branch env)
             ; empty else case
-            'no-else)))
+            'no-else))))
 
-(define (eval-visit-lit value env v)
-  value)
+(define (eval-visit-lit value wrong-env v)
+  (lambda (env) value))
 
-(define (eval-visit-ident name env v)
-  (lookup env name))
+(define (eval-visit-ident name wrong-env v)
+  (lambda (env)
+    (lookup env name)))
 
-(define (eval-visit-expr-list-init) '())
+(define (eval-visit-expr-list-init) (void))
 
 (define (eval-visit-reduce-expr-list expr-result acc) expr-result) ; only keep latest result
+
+(define (eval-visit-toplevel-init) (empty-env))
+
+(define (eval-visit-toplevel-reduce name expr-result acc-env)
+  (bind-env name (expr-result acc-env) acc-env))
 
 (define eval-visitor (visitor
                       eval-visit-funcall
@@ -49,10 +60,12 @@
                       eval-visit-lit
                       eval-visit-ident
                       eval-visit-expr-list-init
-                      eval-visit-reduce-expr-list))
+                      eval-visit-reduce-expr-list
+                      eval-visit-toplevel-init
+                      eval-visit-toplevel-reduce))
 
 (define (eval-expr expr env)
-  (visit-expr expr env eval-visitor))
+  ((visit-expr expr 'fake-env eval-visitor) env))
 
 (define (eval-toplevel expr [starting-env (empty-env)])
   (visit-toplevel expr starting-env eval-visitor))
@@ -76,44 +89,47 @@
                 "trivial if true")
 
   (check-equal? (eval-expr (ifexpr (ident "f") (lit 1) (lit 2))
-                         (bind-env "f" 0 (empty-env)))
+                           (bind-env "f" 0 (empty-env)))
                 2
                 "if var")
 
   (check-equal? (eval-expr (ident "f")
-                           (eval-toplevel (parse-string
-                              "let g = 1 let g = 2 let f = g")))
+                           (eval-toplevel
+                             (parse-string
+                               "let g = 1 let g = 2 let f = g")))
                 2
                 "sequential let")
 
-  (check-equal? (eval-expr (ident "f")
-                           (eval-toplevel (parse-string
-                              "def g(a) if a then 42 else 17 end end let f = g(1)")))
-                42
-                "basic fun call")
+   (check-equal? (eval-expr
+                   (ident "f")
+                   (eval-toplevel
+                    (parse-string
+                      "def g(a) if a then 42 else 17 end end let f = g(1)")))
+                 42
+                 "basic fun call")
 
-  (check-equal? (eval-expr (funcall (ident "g") '())
-                           (eval-toplevel (parse-string "let g = fun() (17) end")))
-                17
-                "fun call no args")
+   (check-equal? (eval-expr (funcall (ident "g") '())
+                            (eval-toplevel (parse-string "let g = fun() (17) end")))
+                 17
+                 "fun call no args")
 
-  (check-equal? (eval-expr (funcall (ident "g") '())
-                           (eval-toplevel (parse-string "let g = fun() if 4 then 3 else 5 end 42 end")))
-                42
-                "fun call multiple body expressions")
+   (check-equal? (eval-expr (funcall (ident "g") '())
+                            (eval-toplevel (parse-string "let g = fun() if 4 then 3 else 5 end 42 end")))
+                 42
+                 "fun call multiple body expressions")
 
-  (check-equal? (eval-expr (funcall (ident "g") '())
-                           (eval-toplevel (parse-string
-                                           "def h(x) fun() x end end
-                                            let g = h(19)"))) ; g = fun() 19 end
-                19
-                "closure")
+   (check-equal? (eval-expr (funcall (ident "g") '())
+                            (eval-toplevel (parse-string
+                                            "def h(x) fun() x end end
+                                             let g = h(19)"))) ; g = fun() 19 end
+                 19
+                 "closure")
 
-  (check-equal? (eval-expr (funcall (ident "g") (list (lit 3) (ident "x")))
-                           (eval-toplevel (parse-string
-                                           "def h(x) fun(a b) b end end
-                                            let x = 5
-                                            let g = h(19)"))) ; g = fun(a b) b end
-                5
-                "multiple args")
+   (check-equal? (eval-expr (funcall (ident "g") (list (lit 3) (ident "x")))
+                            (eval-toplevel (parse-string
+                                            "def h(x) fun(a b) b end end
+                                             let x = 5
+                                             let g = h(19)"))) ; g = fun(a b) b end
+                 5
+                 "multiple args")
 )
--- visit.rkt
+++ visit.rkt
@@ -11,6 +11,8 @@
                  ident   ; (name env v)
                  expr-list-init    ; () -> acc, initial acc for reduce-expr-list
                  reduce-expr-list  ; (expr-result acc) -> acc
+                 toplevel-init     ; () -> acc
+                 toplevel-reduce   ; (name, expr-result, acc) -> acc
                  ))
 
 ; eval a list of exprs, accumulating results into an accumulator via reduce-expr-list
@@ -27,18 +29,18 @@
     [(funcall? syntax)
      ((visitor-funcall v)
       (visit-expr (funcall-fun syntax) env v)
-      (map (lambda (arg-expr) (visit-expr arg-expr env v))
+      (map (lambda (arg-expr)
+             (visit-expr arg-expr env v))
            (funcall-args syntax))
       env v)]
     [(fundef? syntax)
      (visit-fundef syntax env v)]
     [(ifexpr? syntax)
-     ; eagerly eval condition, pass branches as thunks env->result
      ((visitor-ifexpr v)
       (visit-expr (ifexpr-condition syntax) env v)
-      (lambda (env) (visit-expr (ifexpr-true syntax) env v))
+      (visit-expr (ifexpr-true syntax) env v)
       (if (ifexpr-else syntax)
-          (lambda (env) (visit-expr (ifexpr-else syntax) env v))
+          (visit-expr (ifexpr-else syntax) env v)
           #f)
       env v)]
     [(lit? syntax)
@@ -51,14 +53,14 @@
   ; body as thunk env->result
   ((visitor-fundef v)
    (map ident-name (fundef-args syntax))
-   (lambda (env) (visit-expr-list (fundef-body syntax) env v))
+   (visit-expr-list (fundef-body syntax) env v)
    env v))
 
 ; evaluate a toplevel into an env itself
 ; take for granted the usual toplevel scoping logic
 (define (visit-toplevel syntax env v)
   (let loopy ([toplevel-remaining syntax]
-              [curr-env env])
+              [curr-env ((visitor-toplevel-init v))])
     (if (empty? toplevel-remaining)
         curr-env  ; done
         (let ([item (car toplevel-remaining)])
@@ -67,24 +69,15 @@
              (let ([var   (ident-name (toplevel-let-name item))]
                    [value (visit-expr (toplevel-let-value item) curr-env v)])
                (loopy (cdr toplevel-remaining)
-                      (bind-env var value curr-env)))]
+                      ((visitor-toplevel-reduce v) var value curr-env)))]
             [(toplevel-def? item)
-             (let ([id (ident-name (toplevel-def-name item))]
-                   [f (toplevel-def-value item)])
+             (let* ([var (ident-name (toplevel-def-name item))]
+                   [f (toplevel-def-value item)]
+                   [value (visit-fundef f curr-env v)])
                (loopy (cdr toplevel-remaining)
-                      (bind-env id (visit-fundef f curr-env v) curr-env)))])))))
+                      ((visitor-toplevel-reduce v) var value curr-env)))])))))
 
 (module+ main
-
-  (define (get-depth env)
-    (if (empty? env)
-        0
-        (if (equal? (car (car env)) 'depth-key)
-            (cdr (car env))
-            (get-depth env))))
-
-  (define (set-depth value env)
-    (bind-env 'depth-key value env))
 
   (define (incr n) (+ n 1))
 
@@ -103,7 +96,7 @@
   (define (print-fundef args body-thunk env v)
     (lambda (out depth)
       (display (format "fun ~a\n" args) out)
-      ((body-thunk env) out (incr depth))
+      (body-thunk out (incr depth))
       (display (format "~aend\n" (indent depth)) out)))
 
   (define (print-ifexpr cond-result true-thunk else-thunk-or-false env v)
@@ -111,11 +104,11 @@
         (display (format "~aif(" (indent depth)) out)
         (cond-result out depth)
         (display ") then\n" out)
-        ((true-thunk env) out (incr depth))
+        (true-thunk out (incr depth))
         (if (not (false? else-thunk-or-false))
             (begin
               (display (format  "~aelse\n" (indent depth)) out)
-              ((else-thunk-or-false env) out (incr depth)))
+              (else-thunk-or-false out (incr depth)))
             (void))
         (display (format "~aend\n" (indent depth)) out)))
 
@@ -130,21 +123,35 @@
                            print-ifexpr
                            print-lit
                            print-ident
+                           ; expr-list reduce init
                            (lambda () (lambda (out depth) (void)))
+                           ; expr list reduce
                            (lambda (expr-result acc)
                              (lambda (out depth)
                                (acc out depth)
                                (expr-result out depth)))
+                           ; toplevel init
+                           (lambda () (lambda (out depth) (void)))
+                           ; toplevel reduce
+                           (lambda (name item-thunk remaining-thunk)
+                             (lambda (out depth)
+                               (display (format "~alet ~a = " (indent depth) name))
+                               (item-thunk out depth)
+                               (remaining-thunk out depth)
+                             ))
                            ))
 
   (define parsed (parse (open-input-string "let abc = if x then (3) else print(5) end let x= fun(z) print(3) plus(3 plus(2 1)) end let t = 2")))
   (define toplevel-result (visit-toplevel parsed (empty-env) printer))
-  (let loop ((remaining toplevel-result))
-    (if (not (empty? remaining))
-        (let ((curr (car remaining)))
-          (loop (cdr remaining))
-          (display (format "let ~a = " (car curr)))
-          ((cdr curr) (current-output-port) 0))
-        #f))
+
+  (toplevel-result (current-output-port) 0)
+
+;;   (let loop ((remaining toplevel-result))
+;;     (if (not (empty? remaining))
+;;         (let ((curr (car remaining)))
+;;           (loop (cdr remaining))
+;;           (display (format "let ~a = " (car curr)))
+;;           ((cdr curr) (current-output-port) 0))
+;;         #f))
 
 )
