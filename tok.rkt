1642a19282db7b2bb40a607c2b7c90fd6fb8c534
Andrew Fleenor <andrewflnr@gmail.com>
2026-04-30 10:53:48 -0400

    cleaner, data-driven tokenizer
    tokens defined by structs of predicates and a flush function.
    Iterate through a loop of them to dispatch token types.

--- tok.rkt
+++ tok.rkt
@@ -4,82 +4,105 @@
 
 (provide token token? token-kind token-text tokenize token-sequence)
 
+(struct tokendef
+  (flush start? continue? error?))
+
+(define (false-pred p) #f)
+
+(define (make-tokendef flush-fn
+                       start-pred
+                       [continue-pred false-pred]
+                       [error-pred false-pred])
+  (tokendef flush-fn start-pred continue-pred error-pred))
+
 (struct token (kind text) #:transparent)
 
-(define (parse-token text-stream
-                     flush-fn
-                     start-pred?
-                     continue-pred?
-                     [error-pred? (lambda (p) #f)])
+(define (copy-char in-stream out-stream) (write-char (read-char in-stream) out-stream))
+
+(define (parse-token text-stream td)
   (let* ([so-far (open-output-string)]
-         [copy-char (lambda () (write-char (read-char text-stream) so-far))]
-         [flush (lambda () (flush-fn (get-output-string so-far)))])
-    (if (start-pred? (peek-char text-stream))
+         [flush (lambda () ((tokendef-flush td) (get-output-string so-far)))])
+    (if ((tokendef-start? td) (peek-char text-stream))
         (begin
-          (copy-char) ; incl first char
+          (copy-char text-stream so-far) ; incl matching first char
           (let do-token ()
             (let ([p (peek-char text-stream)])
               (cond
                 [(eof-object? p)
                  (flush)]
-                [(continue-pred? p)
+                [((tokendef-continue? td) p)
                  (begin
-                   (copy-char)
+                   (copy-char text-stream so-far)
                    (do-token))]
-                [(error-pred? p)
+                [((tokendef-error? td) p)
                  (error "lexer error")]
                 [else
+                 ; no errors, no continue, success
                  (flush)]))))
-        (raise "oh noes") )))
+        ; start-pred failed, indicate match failure by returning false
+        #f )))
 
 (define (is-op? c)
   ; surely there must be a better way?
   (string-contains? "=!@$%^&*()[]{},./-+~|" (make-string 1 c)))
 
+(define tokendefs
+  (list
+   ; whitespace
+   (make-tokendef
+    (lambda (s) '())  ; don't emit anything
+    char-whitespace?
+    char-whitespace?)
+  ; comments
+  (make-tokendef
+   (lambda (s) '())  ; don't emit anything
+   (lambda (p) (equal? p #\#))
+   (lambda (p) (not (equal? p #\newline))))
+  ; identifiers, symbols, etc
+  (make-tokendef
+   (lambda (s) (token 'symbolic s))
+   char-alphabetic?
+   (lambda (c) (or (char-alphabetic? c)
+                   (char-numeric? c)
+                   (equal? c #\-)
+                   (equal? c #\_))))
+  ; numbers
+  (make-tokendef
+   (lambda (s) (token 'numeric (or (string->number s) (error (format "invalid numeric '~a'" s)))))
+   char-numeric?
+   (lambda (c) (or (char-numeric? c) (equal? c #\.)))
+   char-alphabetic?) ; no letters allowed without space, operator etc intervening
+  ; operators
+  (make-tokendef
+   (lambda (s) (token 'operator s))
+   is-op?)
+   ; no continue, only single-char operators
+   ))
+
+; look for matching tokens, if any, return their flush value
+; return false if failed to parse
+(define (parse-tokendefs text-stream defs)
+  (if (empty? defs)
+      #f
+      (let* ([curr (car defs)]
+             [result (parse-token text-stream curr)])
+        (or result (parse-tokendefs text-stream (cdr defs))))))
+
 ; return generator of tokens
 (define (tokenize text-stream)
   (generator ()
-
     ; loop body
     (define (per-token)
       (let ([p (peek-char text-stream)])
-        (cond
-          [(eof-object? p)
-           ; exit loop
-           (void)]
-          [(char-whitespace? p)
-           (read-char text-stream) ; throw away
-           (per-token)]
-          [(equal? #\# p)
-           ; ignore result of parse-token
-           (parse-token text-stream
-                        (lambda (s) #f)  ; don't emit anything
-                        (lambda (p) (equal? p #\#))
-                        (lambda (p) (not (equal? p #\newline))))
-           (per-token)]
-          [(char-alphabetic? p)
-           (yield (parse-token text-stream
-                               (lambda (s) (token 'symbolic s))
-                               char-alphabetic?
-                               (lambda (c) (or (char-alphabetic? c)
-                                               (char-numeric? c)
-                                               (equal? c #\-)
-                                               (equal? c #\_)))))
-           (per-token)]
-          [(char-numeric? p)
-           (yield (parse-token text-stream
-                               (lambda (s) (token 'numeric (or (string->number s) (error (format "invalid numeric '~a'" s)))))
-                               char-numeric?
-                               (lambda (c) (or (char-numeric? c) (equal? c #\.)))
-                               char-alphabetic?)) ; no letters allowed without space, operator etc intervening
-           (per-token)]
-          [(is-op? p)
-           (yield (parse-token text-stream
-                               (lambda (s) (token 'operator s))
-                               is-op?
-                               (lambda (t) #f))) ; continue is false, only single char operators
-           (per-token)]
-          [else (error (format "unexpected character ~v" p))])))
+        (if (eof-object? p)
+            ; exit loop
+            (void)
+            (let ([r (parse-tokendefs text-stream tokendefs)])
+              (cond
+                [(token? r) (yield r)]
+                [(empty? r) (void)]
+                [else (error (format "unexpected character ~v" p))])
+              (per-token)))))
     ; start loop
     (per-token)
     ; final token
